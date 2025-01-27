40e97c6e7883890e786d49b04f78afea43709681
JoshuaSingla <joshua.singla@Gmail.com>
2025-01-27 20:19:05 +1000

    overlayed alerts done

--- wh100/frontend/src/Dashboard.js
+++ wh100/frontend/src/Dashboard.js
@@ -160,56 +160,65 @@ const saveVotes = async () => { // update user's votes in database using the `vo
 
   return (
     <div>
-      <h1>Dashboard</h1>
-      <p>Welcome user {user.display}!</p>
-      {showTemporaryAlert && (
-        <TemporaryAlert message={temporaryMessage} duration={5000} key = {alertKey}/>
-      )}
-      {showAlert && (
-        <CustomAlert message={alertMessage} onClose={closeAlert} />
-      )}
-      <form className="max-w-md mx-auto" onSubmit={handleSearch}>
-        <label
-          htmlFor="default-search"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >
-          Search
-        </label>
-        <div className="relative">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
+      <div className = "relative">
+        <h1>Dashboard</h1>
+        <p>Welcome user {user.display}!</p>
+        {/* TemporaryAlert */}
+        {showTemporaryAlert && (
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full max-w-lg z-10">
+            <TemporaryAlert message={temporaryMessage} duration={5000} key={alertKey} />
           </div>
-          <input
-            type="search"
-            id="default-search"
-            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search songs by title, artist, album..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            required
-          />
-          <button
-            type="submit"
-            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        )}
+
+        {/* CustomAlert */}
+        {showAlert && (
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full max-w-lg z-10">
+            <CustomAlert message={alertMessage} onClose={closeAlert} />
+          </div>
+        )}
+        <form className="max-w-md mx-auto" onSubmit={handleSearch}>
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
           >
             Search
-          </button>
-        </div>
-      </form>
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search songs by title, artist, album..."
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              required
+            />
+            <button
+              type="submit"
+              className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
       {/* Render search results */}
       {results && (
         <div className="mx-auto">
