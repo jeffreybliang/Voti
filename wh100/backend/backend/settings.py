c0410a0f887a6a5c4ebddaccd99b60324f46b78d
JoshuaSingla <joshua.singla@Gmail.com>
2025-02-09 23:08:28 +1100

    updated allowed hosts

--- wh100/backend/backend/settings.py
+++ wh100/backend/backend/settings.py
@@ -35,9 +35,13 @@ END_DATE=env("END_DATE")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'woronihottest100-production.up.railway.app',
+]
 
 
 # Application definition
