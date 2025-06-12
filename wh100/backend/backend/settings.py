bc3db06ecaf3b0fe21289e5a02ba6fb65f2b2932
jeffreyliang <u7013004@anu.edu.au>
2025-06-12 21:43:58 +1000

    changed email to noreply

--- wh100/backend/backend/settings.py
+++ wh100/backend/backend/settings.py
@@ -246,8 +246,8 @@ EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
 EMAIL_BACKEND = 'django_ses.SESBackend'
 ACCOUNT_EMAIL_REQUIRED = False
 ACCOUNT_USERNAME_REQUIRED = True
-DEFAULT_FROM_EMAIL = 'woronihottest100@gmail.com'
-# DEFAULT_FROM_EMAIL = 'woroni@woroni100.com'
+# DEFAULT_FROM_EMAIL = 'woronihottest100@gmail.com'
+DEFAULT_FROM_EMAIL = 'noreply@woroni100.com'
 
 HEADLESS_ADAPTER = "accounts.headless_adapter.MyHeadlessAdapter"
 # This is crucial for the form to be used
