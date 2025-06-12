c780a850d71a245cdedf6a36184617665920447a
jeffreyliang <u7013004@anu.edu.au>
2025-06-12 22:24:57 +1000

    changed debug from True to False

--- wh100/backend/backend/settings.py
+++ wh100/backend/backend/settings.py
@@ -43,7 +43,7 @@ AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
