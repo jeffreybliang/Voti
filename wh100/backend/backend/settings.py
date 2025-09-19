49dc7cbf2e2de6c4f05d85f63a98cd874025eee5
jeffreyliang <u7013004@anu.edu.au>
2025-09-19 10:16:23 +1000

    updated settings

--- wh100/backend/backend/settings.py
+++ wh100/backend/backend/settings.py
@@ -166,7 +166,7 @@ TEMPLATES = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": env('REDIS_PUBLIC_URL'),
+        "LOCATION": env('REDIS_PRIVATE_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
